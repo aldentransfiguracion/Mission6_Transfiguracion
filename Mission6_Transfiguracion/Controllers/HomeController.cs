7e351c483d001329758026f701b1003a1954decf
alden <alden@ALDENTRANSFB1A6>
2024-02-16 23:57:01 -0700

    Add project files.

---
+++ Mission6_Transfiguracion/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission6_Transfiguracion.Models;
+using System.Diagnostics;
+
+namespace Mission6_Transfiguracion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AddMovieContext _context;
+        public HomeController(AddMovieContext temp)
+        {
+            _context = temp;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie(AddMovie response)
+        {
+            if (response.LentTo == null)
+            {
+                response.LentTo = "";
+            }
+
+            if (response.Notes == null)
+            {
+                response.Notes = "";
+            }
+            if (response.Category == null)
+            {
+                response.Category = "";
+            }
+            if (response.Title == null)
+            {
+                response.Title = "";
+            }
+            if (response.Director == null)
+            {
+                response.Director = "";
+            }
+            if (response.Rating == null)
+            {
+                response.Rating = "";
+            }
+            _context.AddMovie.Add(response);
+            _context.SaveChanges();
+            return View("Confirmation");
+        }
+    }
+}
---
+++ Mission6_Transfiguracion/Models/AddMovieContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission6_Transfiguracion.Models
+{
+    public class AddMovieContext : DbContext
+    {
+        public AddMovieContext(DbContextOptions<AddMovieContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AddMovie> AddMovies { get; set; }
+    }
+}
